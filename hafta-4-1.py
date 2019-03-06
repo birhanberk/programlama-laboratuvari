b320a87b5e1fd07305ebcdd95829af0636d44f18
birhanberk <birhanberk@gmail.com>
2019-03-06 19:33:02 +0300

    04/03/2019 - Matris işlemleri

--- hafta-4-1.py
+++ hafta-4-1.py
@@ -1,54 +1,82 @@
-import random
+# 2 matrisi toplayan fonksiyon
+def matrix_addition(m_1,m_2):
+  result=[]
+  for row in range(len(m_1)):
+    result.append([])
+    for column in range(len(m_1[0])): #ya da (len(m_1)+1) da olur
+      #print(m_1[row][column],end=" ")
+      result[row].append(m_1[row][column]+m_2[row][column])
+  return result
+  
+# 2 matrisi çıkaran fonksiyon
+def matrix_substraction(m_1,m_2):
+  result=[]
+  for row in range(len(m_1)):
+    result.append([])
+    for column in range(len(m_1[0])):
+      #print(m_1[row][column],end=" ")
+      result[row].append(m_1[row][column]-m_2[row][column])
+  return result
 
-# n elemanlı array oluşturan fonksiyon
-def generate_an_array(n):
-  my_array=[]
-  for i in range(n):
-    s=random.randint(0,100)     # 0 ile 100 arası random sayı oluştur
-    my_array.append(s)          # oluşturalan random sayıyı array e ekle
-  return my_array
+# matrisi skaler çarpan fonksiyon
+def matrix_scalar_product(alpha,m_1):
+  result=[]
+  for row in range(len(m_1)):
+    result.append([])
+    for column in range(len(m_1[0])):
+      #print(m_1[row][column],end=" ")
+      result[row].append(m_1[row][column]*alpha)
+  return result
 
-my_arr_1=generate_an_array(10)
-print(my_arr_1)
-print("\n")
+matrix_1=[[1,2,3], [4,5,6]]
+matrix_2=[[7,3,9], [1,10,8]]
+alpha=10
+print(matrix_scalar_product(alpha,matrix_1))
+print(matrix_addition(matrix_1,matrix_2))
+print(matrix_substraction(matrix_1,matrix_2))
 
-# array elemanlarını iteratif olarak dictionary biçiminde yazdırma
-for i in range(len(my_arr_1)):
-  print(i,":",my_arr_1[i])
-print("\n")
+# i inci satırı döndüren fonksiyon
+def f_1(matrix_1,i):
+  return matrix_1[i]
 
-# array elemanlarını iteratif olarak yanyana yazdıran fonksiyon
-def print_an_array(my_arr_1):
-  for item in my_arr_1:
-    print(item, end=" ")
-        
-print_an_array(my_arr_1)
-print("\n")
+print(f_1(matrix_1,1))
 
-# array elemanlarını bubble sort ile küçükten büyüğe sıralayan fonksiyon
-def my_bubble_sort(my_array):
-  for i in range(len(my_arr_1)-1,0,-1):
-    for j in range(i):
-      if(my_arr_1[j]>my_arr_1[j+1]):
-        t=my_arr_1[j]
-        my_arr_1[j]=my_arr_1[j+1]
-        my_arr_1[j+1]=t
+# j inci sütunu döndüren fonksiyon
+def f_2(matrix_1,j):
+  j_th_col=[]
+  for row in matrix_1:
+    for indis in range(len(row)):
+      if(indis==j):
+        j_th_col.append(row[indis])
+  return j_th_col
 
-my_bubble_sort(my_arr_1)
-print_an_array(my_arr_1)
-print("\n")
+print(f_2(matrix_1,0))
 
-# array içerisinde parametre ile gönderilen değeri arayan fonksiyon
-def search_an_array(my_array,n):
-  found=False
-  step=0                          # aranan sayının kaç adımda bulduğumuzu tutan değişken
-  for i in range(len(my_array)):
-    if(my_array[i]==n):         # aranan sayının kontrol edildiği yer
-      found=True
-      step=i+1
-  if(found==False):
-    print("not found")
-  else:
-    print("found:",found,"step:",step)
+# vektörel iç çarpım
+def vector_inner_product(v,w):
+  size=len(v)
+  result=[]
+  for i in range(size):
+    result.append(0)
+  for i in range(size):
+    result[i]=v[i]*w[i]
+  t=0
+  for i in range(size):
+    t=t+result[i]
+  return t
 
-search_an_array(my_arr_1,100)
+# matris çarpımı
+def matrix_multiply(m_1,m_2):
+  result=[]
+  for row in range(len(m_1)):
+    result.append([])
+    for column in range(len(m_2[0])):
+      a=f_1(m_1,row)
+      b=f_2(m_2,column)
+      c=vector_inner_product(a,b)
+      result[row].append(c)
+  return result
+
+matrix_1=[[1,2,3], [4,5,6]]
+matrix_2=[[7,3], [1,10],[0,2]]
+print(matrix_multiply(matrix_1,matrix_2))
